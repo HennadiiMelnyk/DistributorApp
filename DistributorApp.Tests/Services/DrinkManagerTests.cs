a88ea9b470dbd63cebf1e24cc45bca1b9e65bf43
Hennadii Melnyk <hennadii.melnyk@capgemini.com>
2024-02-19 23:09:32 +0100

    add Test solution

---
+++ DistributorApp.Tests/Services/DrinkManagerTests.cs
@@ -0,0 +1,33 @@
+﻿using DistributorApp.Services;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DistributorApp.Tests.Services;
+
+/// <summary>
+/// Contains tests for <see cref="IDrinkManager"/>
+/// </summary>
+
+public class DrinkManagerTests
+{
+    private IDrinkManager _drinkManager;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        _drinkManager = Substitute.For<IDrinkManager>();
+    }
+
+    //[TestCase("Chocolate")]
+    [Test]
+    public void CreateChocolateDrink()
+    {
+        // Arrange 
+        var chocolateDrink = "";
+        // Act
+        var chocolatePrice = _drinkManager.CalculateDrinkPrice(chocolateDrink);
+        // Assert
+        //Assert.That(chocolatePrice, Is.Not.Null);
+
+    }
+}
